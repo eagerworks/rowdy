3b67b7659c50e517360226b341b8c8bce31541cf
Lucas de sierra <83076120+ldesierra@users.noreply.github.com>
2026-04-21 12:04:15 -0300

    Add schema edition

--- app/controllers/rowdy/schemas_controller.rb
+++ app/controllers/rowdy/schemas_controller.rb
@@ -3,7 +3,7 @@ module Rowdy
     def show
       @schema = SchemaRegistry.find!(params[:schema_name])
       @uploads = Upload.where(schema_name: @schema.schema_name).order(created_at: :desc)
-      # redirect_to import_validation_path(@uploads.first.imports.first)
+      @schema_definition = SchemaDefinition.find(@schema.schema_definition_id) if @schema.dynamic?
     end
 
     private
