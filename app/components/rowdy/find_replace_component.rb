ce7c644fc916a3626e0e57ea8cba61ee36ed9ceb
Lucas de sierra <83076120+ldesierra@users.noreply.github.com>
2026-05-08 10:32:45 -0300

    Fix find and replace redirection

--- app/components/rowdy/find_replace_component.rb
+++ app/components/rowdy/find_replace_component.rb
@@ -1,18 +1,22 @@
 module Rowdy
   class FindReplaceComponent < ViewComponent::Base
-    attr_accessor :import, :tabs
+    attr_accessor :import
 
-    def initialize(import:, tabs: [])
-      @import          = import
-      @tabs = tabs
+    def initialize(import:, tabs: nil, frame_id: nil)
+      @import   = import
+      @frame_id = frame_id
     end
 
     def replace_all_path
       Rowdy::Engine.routes.url_helpers.replace_all_import_path(@import)
     end
 
+    def columns
+      ErroredColumnsQuery.call(@import)
+    end
+
     def frame_id
-      @import.steps_frame_id
+      @frame_id || @import.steps_frame_id
     end
   end
 end
